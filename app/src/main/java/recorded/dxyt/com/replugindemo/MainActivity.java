1f3ce270ea8bc0cc5601fd5dc5a7eef769aafab7
RookieBro <18592054972@163.com>
2019-08-21 10:09:26 +0800

    修改了部分配置

--- app/src/main/java/recorded/dxyt/com/replugindemo/MainActivity.java
+++ app/src/main/java/recorded/dxyt/com/replugindemo/MainActivity.java
@@ -1,13 +1,131 @@
 package recorded.dxyt.com.replugindemo;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.qihoo360.replugin.RePlugin;
+import com.qihoo360.replugin.model.PluginInfo;
+import com.qihoo360.replugin.utils.FileUtils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn, installs;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.on_click);
+        installs = findViewById(R.id.install);
+        installs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final ProgressDialog pd = ProgressDialog.show(MainActivity.this, "Installing...", "Please wait...", true, true);
+                // FIXME: 仅用于安装流程演示 2017/7/24
+                new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        simulateInstallExternalPlugin();
+                        pd.dismiss();
+                    }
+                }, 1000);
+            }
+        });
+
+        // 刻意使用Thread的ClassLoader来测试效果
+//        testThreadClassLoader();
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 若没有安装，则直接提示“错误”
+                // TODO 将来把回调串联上
+                if (RePlugin.isPluginInstalled("demo2")) {
+                    RePlugin.startActivity(MainActivity.this, RePlugin.createIntent("demo2", "recorded.dxyt.com.demo2.MainActivity"));
+                } else {
+                    Toast.makeText(MainActivity.this, "You must install demo3 first!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    /**
+     * 模拟安装或升级（覆盖安装）外置插件
+     * 注意：为方便演示，外置插件临时放置到Host的assets/external目录下，具体说明见README</p>
+     */
+    private void simulateInstallExternalPlugin() {
+        String demo3Apk= "demo2.apk";
+        String demo3apkPath = "external" + File.separator + demo3Apk;
+
+        // 文件是否已经存在？直接删除重来
+        String pluginFilePath = getFilesDir().getAbsolutePath() + File.separator + demo3Apk;
+        File pluginFile = new File(pluginFilePath);
+        if (pluginFile.exists()) {
+            FileUtils.deleteQuietly(pluginFile);
+        }
+
+        // 开始复制
+        copyAssetsFileToAppFiles(demo3apkPath, demo3Apk);
+        PluginInfo info = null;
+        if (pluginFile.exists()) {
+            info = RePlugin.install(pluginFilePath);
+        }
+
+        if (info != null) {
+            RePlugin.startActivity(MainActivity.this, RePlugin.createIntent(info.getName(), "recorded.dxyt.com.demo2.MainActivity"));
+        } else {
+            Toast.makeText(MainActivity.this, "安装失败！", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * 从assets目录中复制某文件内容
+     *
+     * @param assetFileName assets目录下的Apk源文件路径
+     * @param newFileName   复制到/data/data/package_name/files/目录下文件名
+     */
+    private void copyAssetsFileToAppFiles(String assetFileName, String newFileName) {
+        InputStream is = null;
+        FileOutputStream fos = null;
+        int buffsize = 1024;
+
+        try {
+            is = this.getAssets().open(assetFileName);
+            fos = this.openFileOutput(newFileName, Context.MODE_PRIVATE);
+            int byteCount = 0;
+            byte[] buffer = new byte[buffsize];
+            while ((byteCount = is.read(buffer)) != -1) {
+                fos.write(buffer, 0, byteCount);
+            }
+            fos.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+                fos.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    private void testThreadClassLoader() {
+        // 在2.1.7及以前版本，如果直接调用此方法，则拿到的ClassLoader可能是PathClassLoader或者为空。有极个别Java库会用到此方法
+        // 这里务必确保：cl == getClassLoader()，才符合预期
+        ClassLoader cl = Thread.currentThread().getContextClassLoader();
+        if (cl != getClassLoader()) {
+            throw new RuntimeException("Thread.current.classLoader != getClassLoader(). cl=" + cl + "; getC=" + getClassLoader());
+        }
     }
 }
